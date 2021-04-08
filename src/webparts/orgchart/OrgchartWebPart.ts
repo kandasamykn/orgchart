7b31bc0b3ff1feda775a49faca48c49ab2cdae69
karuna <karunanithi.kandasamy@nih.gov>
2021-04-07 21:00:16 -0400

    adding project from local

---
+++ src/webparts/orgchart/OrgchartWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'OrgchartWebPartStrings';
+import Orgchart from './components/Orgchart';
+import { IOrgchartProps } from './components/IOrgchartProps';
+
+export interface IOrgchartWebPartProps {
+  description: string;
+}
+
+export default class OrgchartWebPart extends BaseClientSideWebPart<IOrgchartWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IOrgchartProps> = React.createElement(
+      Orgchart,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
